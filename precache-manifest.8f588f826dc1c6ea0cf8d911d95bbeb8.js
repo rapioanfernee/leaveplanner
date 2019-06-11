9748fdb6a98b38d54564a7569529a302761ee712
Anfernee Rapio <rapioanfernee@gmail.com>
2019-06-11 22:46:40 +0800

    Updates

---
+++ precache-manifest.8f588f826dc1c6ea0cf8d911d95bbeb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39da70f08ade3bd71af0",
+    "url": "/leaveplanner/static/css/main.4431d31b.chunk.css"
+  },
+  {
+    "revision": "39da70f08ade3bd71af0",
+    "url": "/leaveplanner/static/js/main.0ece628f.chunk.js"
+  },
+  {
+    "revision": "ff921acdd731a82aaaad",
+    "url": "/leaveplanner/static/js/runtime~main.56aa151b.js"
+  },
+  {
+    "revision": "968d262f81763b30f2b3",
+    "url": "/leaveplanner/static/css/2.2e6f8fff.chunk.css"
+  },
+  {
+    "revision": "968d262f81763b30f2b3",
+    "url": "/leaveplanner/static/js/2.156ef37a.chunk.js"
+  },
+  {
+    "revision": "38554275e54b1b66d273561179301ba0",
+    "url": "/leaveplanner/index.html"
+  }
+];
